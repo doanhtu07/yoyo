1f2d40cc960952b3118476d1f5d52665a1e74a24
doanhtu07 <doanhtu07@gmail.com>
2022-09-25 17:59:32 -0500

    Change port to serve on Heroku and setup ping

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import express from "express";
 import path from "path";
 import { COMMANDS } from "./app/commands.js";
+import cron from "cron";
+import axios from "axios";
 
 // https://developer.okta.com/blog/2018/11/15/node-express-typescript
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const CronJob = cron.CronJob;
+const pingHerokuServer = new CronJob("0 */10 * * * *", function () {
+  console.log("Ping at: ", new Date().toString());
+  axios
+    .get("https://yoyo-shortcut.herokuapp.com/")
+    .then(() => {
+      console.log("Successfully ping!");
+    })
+    .catch((err) => {
+      console.error(new Error(String(err)));
+    });
+});
+// That means we are on a server
+if (port !== 3000) {
+  pingHerokuServer.start();
+}
 
 // Static files served inside HTML
 app.use("/public", express.static(path.join(__dirname, "public")));
@@ -32,5 +51,5 @@ app.get("/", (req, res) => {
 
 // start the express server
 app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
+  console.log(`Server started at port ${port}.`);
 });
